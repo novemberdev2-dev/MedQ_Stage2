8b0560f5088109d9fdbbde2eaae299f98adcdd1a
novemberdev2-dev <november.dev2@gmail.com>
2026-07-08 12:21:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -231,7 +231,7 @@ const ALL_SECTIONS = [
     ] },
      { title: '2025-2026 Year Exam',     links: [
       { label: 'End-Block _ Theory', badge: '30 MCQs',  href: 'IBS_End-Block_2025-2026.html' },
-      { label: 'Final', badge: '0 MCQs',  href: 'IBS_Final_2025-2026.html' }
+      { label: 'Final', badge: '113 MCQs',  href: 'IBS_Final_2025-2026.html' }
     
     ] },
     ]
