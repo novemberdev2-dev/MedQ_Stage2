ab30d8959bebbe997010f87294934541530dce92
novemberdev2-dev <november.dev2@gmail.com>
2026-07-11 18:33:58 +0300

    Update exam titles and add unknown year exam

--- script.js
+++ script.js
@@ -315,10 +315,17 @@ const ALL_SECTIONS = [
       ] },
 
 
-     { title: '2025-2026 Year Exam',     links: [
+     { title: '2025-2026 Year Exam 🔥',     links: [
       { label: 'End-Block _ Theory', badge: '29 MCQs',  href: 'CVS_End-Block_Theory_2025-2026.html' },
       { label: 'End-Block _ Practice', badge: '12 Questions',  href: 'CVS_End-Block_Practice_2025-2026.html' },
       { label: 'Final', badge: '54 MCQs',  href: 'CVS_Final_2025-2026.html' }
+    
+    ] },
+
+         { title: 'Unkown Year Exam',     links: [
+      { label: 'End-Block _ Theory', badge: '0 MCQs',  href: 'CVS_End-Block_Theory_2025-2026.html' },
+      { label: 'End-Block _ Practice', badge: '0 Questions',  href: 'CVS_End-Block_Practice_2025-2026.html' },
+      { label: 'Final', badge: '0 MCQs',  href: 'CVS_Final_2025-2026.html' }
     
     ] },
     ]
