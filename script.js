9ce3eb25a489c76c8d6d2b5a110faac8e58e6d4f
novemberdev2-dev <november.dev2@gmail.com>
2026-08-02 12:45:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -331,7 +331,7 @@ const ALL_SECTIONS = [
      
 
 
- { title: '2024-2025 Year Exam',     links: [
+ { title: '2024-2025 Year Exam ✅',     links: [
         { label: 'End-Block _ Theory', badge: '22 MCQs',  href: 'HP_End-Block_Theory_2024-2025.html' },
         { label: 'End-Block _ Practice', badge: '12 Qs',  href: 'HP_End-Block_Practice_2024-2025.html' },
         { label: 'Final', badge: '29 MCQs',  href: 'HP_Final_2024-2025.html' },
